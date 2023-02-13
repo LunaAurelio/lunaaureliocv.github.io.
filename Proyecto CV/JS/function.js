af331a0804cc14afcfd0a39222cbc1419cb9b45e
LunaAurelio <125081488+LunaAurelio@users.noreply.github.com>
2023-02-13 10:32:09 -0300

    Add files via upload

---
+++ Proyecto CV/JS/function.js
@@ -0,0 +1,4 @@
+document.getElementById ("boton").style.display='block' = function () {
+    console.log ("intento hacer que esto funcioneee")
+    document.getElementById ("demo").innerHTML = "HOLA"
+}
